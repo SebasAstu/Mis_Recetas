605a3ab369adaeeabd5d0fb15df0b930ff5d8224
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2023-12-12 00:46:38 -0400

    modificaciones en la pantalla de inicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
-import 'package:mis_recetas/RecipeDetails.dart';
-import 'package:mis_recetas/login.dart';
-import 'package:mis_recetas/login_cubit.dart';
-import 'package:mis_recetas/sign_up_cubit.dart';
-import 'package:mis_recetas/sign_up_page.dart';
 import 'dart:convert';
+
+import 'RecipeDetails.dart';
+import 'login.dart';
+import 'login_cubit.dart';
+import 'sign_up_cubit.dart';
+import 'sign_up_page.dart';
 import 'porfile.dart';
 
 void main() {
@@ -67,12 +68,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Recipe> recipes = [];
+  List<Recipe> featuredRecipes = [];
   bool isLoading = true;
+  bool isLoadingFeaturedRecipes = true;
 
   @override
   void initState() {
     super.initState();
     fetchRecipes();
+    fetchFeaturedRecipes();
   }
 
   Future<void> fetchRecipes() async {
@@ -111,70 +115,126 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  @override
+  Future<void> fetchFeaturedRecipes() async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+            'https://api.edamam.com/search?q=featured&app_id=79acdb3b&app_key=6ec27f72236d11ef5c1820419c9ef05d&from=0&to=5'),
+      );
+
+      print('API Response (Featured Recipes): ${response.body}');
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        setState(() {
+          featuredRecipes = (data['hits'] as List).map((hit) {
+            final recipe = hit['recipe'];
+            return Recipe(
+              label: recipe['label'],
+              imageUrl: recipe['image'],
+              source: recipe['source'],
+              score: recipe['yield'] * recipe['totalTime'],
+              url: recipe['url'],
+            );
+          }).toList();
+          isLoadingFeaturedRecipes = false;
+        });
+      } else {
+        throw Exception('Failed to load featured recipes');
+      }
+    } catch (e) {
+      print('Error loading featured recipes: $e');
+      setState(() {
+        isLoadingFeaturedRecipes = false;
+      });
+    }
+  }
+
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Recetas App'),
         backgroundColor: Color(0xFFFFA53D),
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: recipes.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.network(
-                        recipes[index].imageUrl,
-                        errorBuilder: (BuildContext context, Object error,
-                            StackTrace? stackTrace) {
-                          return Image.asset(
-                              'assets/imagen_reserva.jpg'); // Reemplaza 'imagen_reserva.jpg' con la ruta de tu imagen de reserva.
-                        },
-                      ),
-                      ListTile(
-                        title: Text(recipes[index].label),
-                        subtitle: Text('Fuente: ${recipes[index].source}'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                                'Puntuación: ${recipes[index].score.toStringAsFixed(2)}'),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Implementa la onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => RecipeDetailPage(
-                                          recipe: recipes[index])),
-                                );
-                              },
-                              child: Text('Ver Más'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+      body: Column(
+        children: [
+          
+          isLoadingFeaturedRecipes
+              ? Center(child: CircularProgressIndicator())
+              : _buildFeaturedRecipesSection(),
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Expanded(
+                  child: Center(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: recipes.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center( 
+                                child: Image.network(
+                                  recipes[index].imageUrl,
+                                  errorBuilder:
+                                      (BuildContext context, Object error,
+                                          StackTrace? stackTrace) {
+                                    return Image.asset(
+                                        'assets/imagen_reserva.jpg');
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: Text(recipes[index].label),
+                                subtitle:
+                                    Text('Fuente: ${recipes[index].source}'),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                        'Puntuación: ${recipes[index].score.toStringAsFixed(2)}'),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                RecipeDetailPage(
+                                                    recipe: recipes[index]),
+                                          ),
+                                        );
+                                      },
+                                      child: Text('Ver Más'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                );
-              },
-            ),
+                ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Inicio',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.book),
-            label: 'Mis recentas',
+            label: 'Mis recetas',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
@@ -210,6 +270,70 @@ class _HomePageState extends State<HomePage> {
               break;
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildFeaturedRecipesSection() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recetas Destacadas',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            height: 150.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: featuredRecipes.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecipeDetailPage(
+                          recipe: featuredRecipes[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(right: 16.0),
+                    width: 120.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            featuredRecipes[index].imageUrl,
+                            width: 120.0,
+                            height: 90.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          featuredRecipes[index].label,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main3.dart
+++ lib/main3.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'RecipeDetails.dart';
 import 'porfile.dart';
-
-void main() {
-  runApp(MyApp());
-}
 
 class Recipe {
   final String label;
@@ -24,13 +21,12 @@ class Recipe {
   });
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() {
+  runApp(
+    MaterialApp(
       home: HomePage(),
-    );
-  }
+    ),
+  );
 }
 
 class HomePage extends StatefulWidget {
@@ -40,18 +36,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Recipe> recipes = [];
+  List<Recipe> featuredRecipes = [];
   bool isLoading = true;
+  bool isLoadingFeaturedRecipes = true;
+
+  List<String> categories = ['Pasta', 'Vegetarian', 'Dessert', 'Chicken'];
 
   @override
   void initState() {
     super.initState();
     fetchRecipes();
+    fetchFeaturedRecipes();
   }
 
   Future<void> fetchRecipes() async {
     try {
       final response = await http.get(
-        Uri.parse('https://api.edamam.com/search?q=chicken&app_id=79acdb3b&app_key=6ec27f72236d11ef5c1820419c9ef05d&from=0&to=10&calories=591-722&health=alcohol-free'),
+        Uri.parse(
+            'https://api.edamam.com/search?q=chicken&app_id=79acdb3b&app_key=6ec27f72236d11ef5c1820419c9ef05d&from=0&to=10&calories=591-722&health=alcohol-free'),
       );
 
       print('API Response: ${response.body}');
@@ -60,18 +62,16 @@ class _HomePageState extends State<HomePage> {
         final Map<String, dynamic> data = json.decode(response.body);
 
         setState(() {
-          recipes = (data['hits'] as List)
-              .map((hit) {
-                final recipe = hit['recipe'];
-                return Recipe(
-                  label: recipe['label'],
-                  imageUrl: recipe['image'],
-                  source: recipe['source'],
-                  score: recipe['yield'] * recipe['totalTime'],
-                  url: recipe['url'],
-                );
-              })
-              .toList();
+          recipes = (data['hits'] as List).map((hit) {
+            final recipe = hit['recipe'];
+            return Recipe(
+              label: recipe['label'],
+              imageUrl: recipe['image'],
+              source: recipe['source'],
+              score: recipe['yield'] * recipe['totalTime'],
+              url: recipe['url'],
+            );
+          }).toList();
           isLoading = false;
         });
       } else {
@@ -85,6 +85,42 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> fetchFeaturedRecipes() async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+            'https://api.edamam.com/search?q=featured&app_id=79acdb3b&app_key=6ec27f72236d11ef5c1820419c9ef05d&from=0&to=5'),
+      );
+
+      print('API Response (Featured Recipes): ${response.body}');
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        setState(() {
+          featuredRecipes = (data['hits'] as List).map((hit) {
+            final recipe = hit['recipe'];
+            return Recipe(
+              label: recipe['label'],
+              imageUrl: recipe['image'],
+              source: recipe['source'],
+              score: recipe['yield'] * recipe['totalTime'],
+              url: recipe['url'],
+            );
+          }).toList();
+          isLoadingFeaturedRecipes = false;
+        });
+      } else {
+        throw Exception('Failed to load featured recipes');
+      }
+    } catch (e) {
+      print('Error loading featured recipes: $e');
+      setState(() {
+        isLoadingFeaturedRecipes = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,46 +128,17 @@ class _HomePageState extends State<HomePage> {
         title: Text('Recetas App'),
         backgroundColor: Color(0xFFFFA53D),
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: recipes.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.network(
-                        recipes[index].imageUrl,
-                        errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                          return Image.asset('assets/imagen_reserva.jpg'); // Reemplaza 'imagen_reserva.jpg' con la ruta de tu imagen de reserva.
-                        },
-                      ),
-                      ListTile(
-                        title: Text(recipes[index].label),
-                        subtitle: Text('Fuente: ${recipes[index].source}'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Puntuación: ${recipes[index].score.toStringAsFixed(2)}'),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Implementa la lógica para abrir más detalles de la receta.
-                              },
-                              child: Text('Ver Más'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-            bottomNavigationBar: BottomNavigationBar(
+      body: Column(
+        children: [
+          isLoadingFeaturedRecipes
+              ? Center(child: CircularProgressIndicator())
+              : _buildFeaturedRecipesSection(),
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : _buildCategoriesSection(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -139,7 +146,7 @@ class _HomePageState extends State<HomePage> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.book),
-            label: 'Mis recentas',
+            label: 'Mis recetas',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
@@ -161,18 +168,166 @@ class _HomePageState extends State<HomePage> {
               print('Presionado Perfil');
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => ProfilePage(
-                  username: 'usuario1',
-                  name: 'Juan',
-                  age: 25,
-                  address: 'Calle Principal, 123',
-                  email: 'email',
-                  bio: 'bio',
-                ),),
+                MaterialPageRoute(
+                  builder: (context) => ProfilePage(
+                    username: 'usuario1',
+                    name: 'Juan',
+                    age: 25,
+                    address: 'Calle Principal, 123',
+                    email: 'email',
+                    bio: 'bio',
+                  ),
+                ),
               );
               break;
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildFeaturedRecipesSection() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recetas Destacadas',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            height: 150.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: featuredRecipes.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecipeDetailPage(
+                          recipe: featuredRecipes[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(right: 16.0),
+                    width: 120.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            featuredRecipes[index].imageUrl,
+                            width: 120.0,
+                            height: 90.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          featuredRecipes[index].label,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategoriesSection() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Categorías',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            height: 80.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(right: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Agrega lógica para mostrar recetas de la categoría seleccionada
+                    },
+                    child: Text(categories[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecipeDetailPage extends StatelessWidget {
+  final Recipe recipe;
+
+  RecipeDetailPage({required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe.label),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              recipe.imageUrl,
+              width: 200.0,
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Fuente: ${recipe.source}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Puntuación: ${recipe.score.toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Volver'),
+            ),
+          ],
+        ),
       ),
     );
   }
